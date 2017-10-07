a4416393ef52171a94e85ee4e0112e963e9c1003
shahzeb1 <shahzeb001@gmail.com>
2017-10-06 21:27:04 -0700

    Removed aws.json

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const express = require('express'),
 
 const app = express();
 
-AWS.config.loadFromPath('./aws.json');
+// AWS.config.loadFromPath('./aws.json');
 const ddb = new AWS.DynamoDB();
 
 const redisHost = process.env.REDIS_HOST;
